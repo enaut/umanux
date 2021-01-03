093d8d19ce5b64d5b2f07021f3938c3a667b019d
Dietrich <dietrich@teilgedanken.de>
2021-01-03 11:43:26 +0100

    add some comments

--- src/userlib/files/oplog/mod.rs
+++ src/userlib/files/oplog/mod.rs
@@ -3,14 +3,17 @@ use crate::{UserDBLocal, UserLibError};
 use super::FileContents;
 
 pub trait ExecutableAtom {
+    /// A executable atom that adds, removes or modifies one line in file.
     fn execute(self, content: String) -> Result<String, UserLibError>;
 }
 
 pub trait ExecutableUnit {
+    /// A executable Action that combines mutliple Atoms to a sensible activity.
     fn execute(self, contents: FileContents) -> Result<FileContents, UserLibError>;
 }
 
 pub trait ValidatableUnit {
+    /// ValidatableUnits can validate the state to see if they are at all aplicable.
     fn validate(self, contents: FileContents, db: &UserDBLocal) -> Result<(), UserLibError>;
 }
 
